f9f90f67ffee9e2bd1cf9b74bc78d26d3beb20ff
Sean Lee <seanlee@Seans-MacBook-Pro.local>
2023-05-03 16:44:05 -0400

    add middleware

--- vtuber-api/server.js
+++ vtuber-api/server.js
@@ -18,6 +18,11 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.get('/streams', async (req, res) => {
   try {
     const tokenResponse = await axios.post(`https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`);
