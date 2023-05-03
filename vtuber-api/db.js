1ed34a1f5eba0132d6a99a963cdbe9b8d00dd8b5
Sean Lee <seanlee@Seans-MacBook-Pro.local>
2023-05-03 16:56:41 -0400

    add post route to add json to database

--- vtuber-api/db.js
+++ vtuber-api/db.js
@@ -25,6 +25,76 @@ app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the JSON API!' });
 });
 
+app.post('http://localhost:3001/streams', async (req, res) => {
+  const {
+    id,
+    user_login,
+    user_name,
+    game_name,
+    type,
+    title,
+    viewer_count,
+    started_at,
+    language,
+    thumbnail_url,
+    is_mature,
+  } = req.body;
+
+  // Store the synchronization timestamp
+  const syncTimestamp = new Date();
+
+  try {
+    // Begin a transaction
+    await pool.query('BEGIN');
+
+    // Insert or update the main stream data
+    await pool.query(
+      `INSERT INTO streams (id, user_login, user_name, game_name, type, title, viewer_count, started_at, language, thumbnail_url, is_mature, last_updated)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
+      ON CONFLICT (id) DO UPDATE SET 
+        user_login = EXCLUDED.user_login,
+        user_name = EXCLUDED.user_name,
+        game_name = EXCLUDED.game_name,
+        type = EXCLUDED.type,
+        title = EXCLUDED.title,
+        viewer_count = EXCLUDED.viewer_count,
+        started_at = EXCLUDED.started_at,
+        language = EXCLUDED.language,
+        thumbnail_url = EXCLUDED.thumbnail_url,
+        is_mature = EXCLUDED.is_mature,
+        last_updated = EXCLUDED.last_updated`,
+      [
+        id,
+        user_login,
+        user_name,
+        game_name,
+        type,
+        title,
+        viewer_count,
+        started_at,
+        language,
+        thumbnail_url,
+        is_mature,
+        syncTimestamp,
+      ]
+    );
+
+    // Commit the transaction
+    await pool.query('COMMIT');
+
+    // Remove entries that were not updated during this synchronization
+    await pool.query('DELETE FROM streams WHERE last_updated < $1', [syncTimestamp]);
+
+    res.status(201).json({ message: 'Stream data synchronized successfully' });
+  } catch (error) {
+    // Rollback the transaction in case of an error
+    await pool.query('ROLLBACK');
+    console.error(error);
+    res.status(500).json({ message: 'An error occurred while synchronizing stream data' });
+  }
+});
+
+
 // Start the server
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
