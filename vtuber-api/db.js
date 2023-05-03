15ef965c5f7b221e13bf908def190e277460872b
Sean Lee <seanlee@Seans-MacBook-Pro.local>
2023-05-03 16:53:43 -0400

    change port

--- vtuber-api/db.js
+++ vtuber-api/db.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
