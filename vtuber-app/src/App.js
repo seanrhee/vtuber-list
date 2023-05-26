f9209134647d5661de331db0cffeeb7c8708584b
Sean Lee <seanlee@Seans-MacBook-Pro.local>
2023-05-25 22:34:00 -0400

    import StreamCard component into app

--- vtuber-app/src/App.js
+++ vtuber-app/src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import './components/SynchronizeButton'
-import SynchronizeButton from './components/SynchronizeButton';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import StreamCard from './StreamCard';  // Make sure to import StreamCard
 
 function App() {
+  const [streams, setStreams] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3002/api/streams');
+        setStreams(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <SynchronizeButton />
+    <div className="stream-cards-container">
+      {streams.map(stream => (
+        <StreamCard key={stream.id} stream={stream} />
+      ))}
     </div>
   );
 }
