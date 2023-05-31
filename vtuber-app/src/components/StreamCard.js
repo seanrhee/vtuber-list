f27cef1cf1efb3b791846e65a90df20f8d959c6f
Sean Lee <seanlee@Seans-MacBook-Pro.local>
2023-05-31 15:33:06 -0400

    add more styling to StreamCard:

--- vtuber-app/src/components/StreamCard.js
+++ vtuber-app/src/components/StreamCard.js
@@ -6,12 +6,12 @@ function StreamCard({ stream }) {
       <div className="thumbnail-name">
         <div className="image-container">
           <img src={stream.thumbnail_url.replace('{width}', '304').replace('{height}', '171')} alt={stream.user_name} />
-          <p className="viewer-count">{stream.viewer_count}</p>
+          <p className="viewer-count">🔴 {stream.viewer_count}</p>
         </div>
         <h2>{stream.user_name}</h2>
       </div>
       <div className="streamcard-info">
-        <h4>{stream.title.length > 35 ? `${stream.title.substring(0, 35)}...` : stream.title}</h4>
+        <h4 className="stream-title">{stream.title.length > 35 ? `${stream.title.substring(0, 35)}...` : stream.title}</h4>
         <p className="game-name">{stream.game_name}</p>
       </div>
     </div>
