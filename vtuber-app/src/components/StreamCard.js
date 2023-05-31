04a696f8bf00d64524ed2aa384221788e50327aa
Sean Lee <seanlee@Seans-MacBook-Pro.local>
2023-05-31 15:24:34 -0400

    fix margin for StreamCard

--- vtuber-app/src/components/StreamCard.js
+++ vtuber-app/src/components/StreamCard.js
@@ -4,13 +4,15 @@ function StreamCard({ stream }) {
   return (
     <div className="stream-card">
       <div className="thumbnail-name">
-        <img src={stream.thumbnail_url.replace('{width}', '304').replace('{height}', '171')} alt={stream.user_name} />
-        <p className="viewer-count">{stream.viewer_count}</p>
+        <div className="image-container">
+          <img src={stream.thumbnail_url.replace('{width}', '304').replace('{height}', '171')} alt={stream.user_name} />
+          <p className="viewer-count">{stream.viewer_count}</p>
+        </div>
         <h2>{stream.user_name}</h2>
       </div>
       <div className="streamcard-info">
         <h4>{stream.title.length > 35 ? `${stream.title.substring(0, 35)}...` : stream.title}</h4>
-        <p>{stream.game_name}</p>
+        <p className="game-name">{stream.game_name}</p>
       </div>
     </div>
   );
