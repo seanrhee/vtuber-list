62f863587e6489d86de355d398cd97f314cdddc6
Sean Lee <seanlee@Seans-MacBook-Pro.local>
2023-06-01 10:13:43 -0400

    add hover effect to streamcard

--- vtuber-app/src/components/StreamCard.js
+++ vtuber-app/src/components/StreamCard.js
@@ -2,17 +2,19 @@ import React from "react";
 
 function StreamCard({ stream }) {
   return (
-    <div className="stream-card">
-      <div className="thumbnail-name">
-        <div className="image-container">
-          <img src={stream.thumbnail_url.replace('{width}', '304').replace('{height}', '171')} alt={stream.user_name} />
-          <p className="viewer-count">🔴 {stream.viewer_count}</p>
+    <div className="card-container">
+      <div className="stream-card">
+        <div className="thumbnail-name">
+          <div className="image-container">
+            <img src={stream.thumbnail_url.replace('{width}', '304').replace('{height}', '171')} alt={stream.user_name} />
+            <p className="viewer-count">🔴 {stream.viewer_count}</p>
+          </div>
+          <h2>{stream.user_name}</h2>
         </div>
-        <h2>{stream.user_name}</h2>
-      </div>
-      <div className="streamcard-info">
-        <h4 className="stream-title">{stream.title.length > 35 ? `${stream.title.substring(0, 35)}...` : stream.title}</h4>
-        <p className="game-name">{stream.game_name}</p>
+        <div className="streamcard-info">
+          <h4 className="stream-title">{stream.title.length > 35 ? `${stream.title.substring(0, 35)}...` : stream.title}</h4>
+          <p className="game-name">{stream.game_name}</p>
+        </div>
       </div>
     </div>
   );
