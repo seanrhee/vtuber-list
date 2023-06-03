58f153bbaac5c88d86b4e26e1609ba2b78f0c3b9
Sean Lee <lee.sean.cw@gmail.com>
2023-06-03 18:19:33 -0400

    add content to StreamCardOverlay

--- vtuber-app/src/components/StreamCardOverlay.js
+++ vtuber-app/src/components/StreamCardOverlay.js
@@ -1,5 +1,16 @@
 import React from "react";
 
 function StreamCardOverlay ({stream, onClose}) {
-  
+  return (
+    <div className="overlay-container">
+      <div className="overlay-content">
+      <button onClick={onClose}>Close</button>
+        <h2>{stream.user_name}</h2>
+        <p className="stream-title">{stream.title}</p>
+        <img src={stream.thumbnail_url.replace('{width}', '304').replace('{height}', '171')} alt={stream.user_name} />
+        <p>Viewers: {stream.viewer_count}</p>
+        <p>Game: {stream.game_name}</p>
+      </div>
+    </div>
+  )
 }
