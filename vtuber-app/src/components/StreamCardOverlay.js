8be686c39011f8be1d76c1d90fe8779564e33269
Sean Lee <lee.sean.cw@gmail.com>
2023-06-03 19:36:53 -0400

    add logic to be able to close overlay by click out of the card

--- vtuber-app/src/components/StreamCardOverlay.js
+++ vtuber-app/src/components/StreamCardOverlay.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 function StreamCardOverlay ({stream, onClose}) {
+  const cardRef = useRef();
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (cardRef.current && !cardRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [onClose])
+
   return (
     <div className="overlay-container">
-      <div className="overlay-content">
-      <button onClick={onClose}>Close</button>
+      <div className="overlay-content" ref={cardRef}>
         <h2>{stream.user_name}</h2>
         <p className="stream-title">{stream.title}</p>
         <img src={stream.thumbnail_url.replace('{width}', '304').replace('{height}', '171')} alt={stream.user_name} />
