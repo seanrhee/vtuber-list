d48131ef33ff71a910899efaf47227ee44b83d9c
Sean Lee <lee.sean.cw@gmail.com>
2023-06-03 22:39:00 -0400

    add styling to overlay

--- vtuber-app/src/components/StreamCardOverlay.js
+++ vtuber-app/src/components/StreamCardOverlay.js
@@ -17,14 +17,23 @@ function StreamCardOverlay ({stream, onClose}) {
     };
   }, [onClose])
 
+
+
   return (
     <div className="overlay-container">
       <div className="overlay-content" ref={cardRef}>
-        <h2>{stream.user_name}</h2>
-        <p className="stream-title">{stream.title}</p>
-        <img src={stream.thumbnail_url.replace('{width}', '304').replace('{height}', '171')} alt={stream.user_name} />
-        <p>Viewers: {stream.viewer_count}</p>
-        <p>Game: {stream.game_name}</p>
+        <div className="overlay-thumbnail-cta">
+          <div className="image-container" onClick={() => window.open(`https://www.twitch.tv/${stream.user_login}`, '_blank')}>
+            <img className="overlay-thumbnail" src={stream.thumbnail_url.replace('{width}', '304').replace('{height}', '171')} alt={stream.user_name} />
+            <p className="viewer-count">🔴 {stream.viewer_count}</p>
+          </div>
+          <p className="overlay-cta">Click on the thumbnail to check out the stream!</p>
+        </div>
+        <div className="overlay-info">
+          <h2>{stream.user_name}</h2>
+          <p className="overlay-stream-title">{stream.title}</p>
+          <p className="game-name">{stream.game_name}</p>
+        </div>
       </div>
     </div>
   )
