6e77d8796ababd40fd712d9a475da7f446461b6d
Sean Lee <lee.sean.cw@gmail.com>
2023-06-04 00:21:01 -0400

    add styling to overlay

--- vtuber-app/src/components/StreamCardOverlay.js
+++ vtuber-app/src/components/StreamCardOverlay.js
@@ -22,21 +22,28 @@ function StreamCardOverlay ({stream, onClose}) {
   return (
     <div className="overlay-container">
       <div className="overlay-content" ref={cardRef}>
-        <div className="overlay-thumbnail-cta">
-          <div className="image-container" onClick={() => window.open(`https://www.twitch.tv/${stream.user_login}`, '_blank')}>
-            <img className="overlay-thumbnail" src={stream.thumbnail_url.replace('{width}', '304').replace('{height}', '171')} alt={stream.user_name} />
-            <p className="viewer-count">🔴 {stream.viewer_count}</p>
+        <div className="overlay-top">
+          <div className="overlay-thumbnail-cta">
+            <div className="image-container" onClick={() => window.open(`https://www.twitch.tv/${stream.user_login}`, '_blank')}>
+              <img className="overlay-thumbnail" src={stream.thumbnail_url.replace('{width}', '304').replace('{height}', '171')} alt={stream.user_name} />
+              <p className="viewer-count">🔴 {stream.viewer_count}</p>
+            </div>
+            <p className="overlay-cta">Click on the thumbnail to check out the stream!</p>
           </div>
-          <p className="overlay-cta">Click on the thumbnail to check out the stream!</p>
+          <div className="overlay-info">
+            <div className="overlay-primary info">
+              <h2>{stream.user_name}</h2>
+              <p className="overlay-stream-title">{stream.title}</p>
+              <p className="game-name">{stream.game_name}</p>
+            </div>
+            <div className="overlay-secondary-info">
+              <p className="overlay-info-text">18+: {stream.is_mature ? 'YES' : "NO"}</p>
+              <p className="overlay-info-text">Language: {stream.language.toUpperCase()}</p>
+            </div>
+          </div>
         </div>
-        <div className="overlay-info">
-          <h2>{stream.user_name}</h2>
-          <p className="overlay-stream-title">{stream.title}</p>
-          <p className="game-name">{stream.game_name}</p>
-          <div className="overlay-secondary-info">
-            <p className="overlay-info-text">18+: {stream.is_mature ? 'YES' : "NO"}</p>
-            <p className="overlay-info-text">Language: {stream.language.toUpperCase()}</p>
-          </div>
+        <div className="overlay-bottom">
+          <h3>tags</h3>
         </div>
       </div>
     </div>
